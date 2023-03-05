8ec1c55eb0bd0c03d7e733cfbc695f0c76f2181e
adsalihac <adsalihac@gmail.com>
2023-03-06 05:17:00 +0530

    installation message changed

--- post-init-script.js
+++ post-init-script.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 
-console.log("This is post init script");
+console.log(
+  "Your boilerplate is ready to go! , you can start coding now! , Happy coding! , Enjoy! , Developed by SH Developers  , Check README.md for more information."
+);
